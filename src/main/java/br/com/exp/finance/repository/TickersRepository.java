d07ef65eb1e5b94579a35594bd0e2815d836cc48
EXPedro <pedroelisio555@gmail.com>
2022-01-27 12:47:28 -0300

    Document repository interfaces

--- src/main/java/br/com/exp/finance/repository/TickersRepository.java
+++ src/main/java/br/com/exp/finance/repository/TickersRepository.java
@@ -3,5 +3,12 @@ package br.com.exp.finance.repository;
 import br.com.exp.finance.model.Tickers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interface that extends JpaRepository
+ * Tickers e Long
+ * @author eXP
+ * @version 0.1
+ */
+
 public interface TickersRepository extends JpaRepository<Tickers, String>{
 }
