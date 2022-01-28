064bf8a27d41d6265bf85a935d360168dc833ad0
EXPedro <pedroelisio555@gmail.com>
2022-01-28 12:32:18 -0300

    Create table Tipo

---
+++ src/main/java/br/com/exp/finance/model/Tipo.java
@@ -0,0 +1,38 @@
+package br.com.exp.finance.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Tipo choose between 1 of the 4 types
+ * that an ativo can be
+ * <p>
+ *     <b>tipo</b>
+ *     String
+ * @author eXP
+ * @version 0.2
+ */
+
+@Entity
+@Table(name="tipo")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id_tipo;
+
+    @Column(nullable = false)
+    private String tipo;
+
+}
